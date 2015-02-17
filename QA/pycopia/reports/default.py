b59c5d061d594b1906fe30e4a8c60fcf8edf9156
Keith Dart <keith@dartworks.biz>
2015-02-16 18:02:59 -0800

    Improved the look of the default report.

--- QA/pycopia/reports/default.py
+++ QA/pycopia/reports/default.py
@@ -21,11 +21,14 @@ Also servers as usage example of TestCase signals.
 """
 
 import sys
+import os
+import pprint
 
 from pycopia.reports import BaseReport
 
+WIDTH, LINES = os.get_terminal_size()
 
-RESET = "\x1b[0m" # aka NORMAL
+RESET = "\x1b[0m"  # aka NORMAL
 
 ITALIC_ON = "\x1b[3m"
 ITALIC_OFF = "\x1b[23m"
@@ -50,7 +53,7 @@ LT_YELLOW = "\x1b[33;01m"
 LT_BLUE = "\x1b[34;01m"
 LT_MAGENTA = "\x1b[35;01m"
 LT_CYAN = "\x1b[36;01m"
-WHITE = "\x1b[01m" # aka INTENSE or BRIGHT,
+WHITE = "\x1b[01m"  # aka INTENSE or BRIGHT,
 
 RED_BACK = "\x1b[41m"
 GREEN_BACK = "\x1b[42m"
@@ -61,42 +64,47 @@ CYAN_BACK = "\x1b[46m"
 WHITE_BACK = "\x1b[47m"
 
 #                 UL  hor   vert  UR  LL   LR
-_BOXCHARS = {1: ['┏', '━', '┃', '┓', '┗', '┛',],
-             2: ['╔', '═', '║', '╗', '╚', '╝',],
-             3: ['┌', '─', '│', '┐', '└', '┘',],
-}
+_BOXCHARS = {1: ['┏', '━', '┃', '┓', '┗', '┛'],
+             2: ['╔', '═', '║', '╗', '╚', '╝'],
+             3: ['┌', '─', '│', '┐', '└', '┘']}
 
 
-_BOXCHARS_A = {1: ['+', '=', '|', '+', '+', '+',],
-               2: ['+', '-', '|', '+', '+', '+',],
-               3: ['+', '+', '|', '+', '+', '+',],
-}
-
+_BOXCHARS_A = {1: ['+', '=', '|', '+', '+', '+'],
+               2: ['+', '-', '|', '+', '+', '+'],
+               3: ['+', '+', '|', '+', '+', '+']}
 
 
 def white(text):
     return WHITE + text + RESET
 
+
 def inverse(text):
     return INVERSE_ON + text + INVERSE_OFF
+
 
 def green(text):
     return GREEN+text+RESET
 
+
 def red(text):
     return RED+text+RESET
+
 
 def blue(text):
     return BLUE + text + RESET
 
+
 def yellow(text):
     return YELLOW+text+RESET
+
 
 def cyan(text):
     return CYAN+text+RESET
 
+
 def lt_red(text):
     return LT_RED+text+RESET
+
 
 def inverse_red(text):
     return INVERSE_ON+RED+text+RESET+INVERSE_OFF
@@ -109,53 +117,78 @@ class DefaultReport(BaseReport):
             file = sys.stdout
         self._file = file
         super().initialize(title)
-        print("{0:^80.80s}".format(inverse(title)), file=file)
+        printer = pprint.PrettyPrinter(stream=file, indent=8, width=WIDTH-16,
+                                       depth=None)
+        self._pprint = printer.pprint
+        print("{I}{0:^{width}.{width}s}{N}".format(title,
+              I=INVERSE_ON, N=INVERSE_OFF, width=WIDTH), file=file)
 
     def finalize(self):
         super().finalize()
-        print("{0}".format(inverse("Done.")), file=self._file)
+        print("{I}{0:^{width}s}{N}".format("Done",
+              I=INVERSE_ON, N=INVERSE_OFF, width=WIDTH), file=self._file)
+        self._pprint = None
         self._file = None
 
     def on_test_start(self, testcase, name=None, time=None):
-        print("  {!s}: start {!s}".format(blue(str(time.time())), name), file=self._file)
+        ts = str(time.time())
+        nw = WIDTH - len(ts) - 7
+        print("\nstart {W}{0:{width}s} {B}{1}{R}".format(name, ts,
+              width=nw, W=WHITE, B=BLUE, R=RESET), file=self._file)
 
     def on_test_end(self, testcase, time=None):
-        print("    {!s}: test end.".format(blue(str(time.time()))), file=self._file)
+        ts = str(time.time())
+        nw = WIDTH - 10
+        print("Test end. {B}{0:>{width}s}{R}".format(ts,
+              width=nw, B=BLUE, R=RESET), file=self._file)
 
     def on_test_passed(self, testcase, message=None):
-        print("    {}: {!s}".format(green("PASSED"), message), file=self._file)
+        print("{}: {!s}".format(green("PASSED"), message), file=self._file)
 
     def on_test_incomplete(self, testcase, message=None):
-        print("    {}: {!s}".format(yellow("INCOMPLETE"), message), file=self._file)
+        print("{}: {!s}".format(yellow("INCOMPLETE"), message),
+              file=self._file)
 
     def on_test_failure(self, testcase, message=None):
-        print("    {}: {!s}".format(red("FAILED"), message), file=self._file)
+        print("{}: {!s}".format(red("FAILED"), message), file=self._file)
 
     def on_test_expected_failure(self, testcase, message=None):
-        print("    {}: {!s}".format(lt_red("EXPECTED FAILED"), message), file=self._file)
+        print("{}: {!s}".format(lt_red("EXPECTED FAILED"), message),
+              file=self._file)
 
     def on_test_abort(self, testcase, message=None):
-        print("    {}: {!s}".format(inverse_red("ABORTED"), message), file=self._file)
+        print("{}: {!s}".format(inverse_red("ABORTED"), message),
+              file=self._file)
 
     def on_test_info(self, testcase, message=None):
-        print("    info: {!s}".format(message), file=self._file)
+        print(" info:", file=self._file)
+        self._pprint(message)
 
     def on_test_diagnostic(self, testcase, message=None):
-        print((MAGENTA+"  diagnostic:"+RESET+" {!s}").format(message), file=self._file)
+        print((MAGENTA+"  diagnostic:"+RESET+" {!s}").format(message),
+              file=self._file)
 
     def on_test_arguments(self, testcase, arguments=None):
         if arguments:
-            print((CYAN+"    arguments:"+RESET+" {!s}").format(arguments), file=self._file)
+            print((CYAN+"    arguments:"+RESET+" {!s}").format(arguments),
+                  file=self._file)
 
     def on_suite_start(self, testsuite, time=None):
-        print("{!s}: suite start {!s}".format(
-                blue(str(time.time())), white(testsuite.test_name)), file=self._file)
+        ts = str(time.time())
+        nw = WIDTH - len(ts) - 12
+        print("\nstart suite {W}{0:{width}s} {B}{1}{R}".format(
+              testsuite.test_name, ts, width=nw, W=WHITE, B=BLUE, R=RESET),
+              file=self._file)
 
     def on_suite_end(self, testsuite, time=None):
-        print("  {!s}: suite end.".format(blue(str(time.time()))), file=self._file)
+        ts = str(time.time())
+        nw = WIDTH - 11
+        print("Suite end. {B}{0:>{width}s}{R}".format(ts,
+              width=nw, B=BLUE, R=RESET), file=self._file)
 
     def on_suite_info(self, testsuite, message=None):
-        print("  info: {!s}".format(message), file=self._file)
+        print("suite info:", file=self._file)
+        self._pprint(message)
 
     def on_run_start(self, runner, timestamp=None):
         print("{!s}: runner start.".format(blue(timestamp)), file=self._file)
@@ -164,7 +197,8 @@ class DefaultReport(BaseReport):
         print("runner end.", file=self._file)
 
     def on_run_arguments(self, runner, message=None):
-        print((CYAN+"runner arguments:"+RESET+" {!s}").format(message), file=self._file)
+        print((CYAN+"runner arguments:"+RESET+" {!s}").format(message),
+              file=self._file)
 
     def on_dut_version(self, testcase, version=None):
         print(("DUT version: {!s}").format(version), file=self._file)
@@ -176,27 +210,26 @@ class DefaultReport(BaseReport):
         print("URL: {!s}: {!s}".format(message, url), file=self._file)
 
 
-
 class DefaultReportUnicode(DefaultReport):
 
     def on_test_passed(self, testcase, message=None):
-        print("    {}: {!s}".format(green('✔'), message), file=self._file)
+        print("{}: {!s}".format(green('✔'), message), file=self._file)
 
     def on_test_incomplete(self, testcase, message=None):
-        print("    {}: {!s}".format(yellow('⁇'), message), file=self._file)
+        print("{}: {!s}".format(yellow('⁇'), message), file=self._file)
 
     def on_test_failure(self, testcase, message=None):
-        print("    {}: {!s}".format(red('✘'), message), file=self._file)
+        print("{}: {!s}".format(red('✘'), message), file=self._file)
 
     def on_test_expected_failure(self, testcase, message=None):
-        print("    {}: {!s}".format(cyan('✘'), message), file=self._file)
+        print("{}: {!s}".format(cyan('✘'), message), file=self._file)
 
     def on_test_abort(self, testcase, message=None):
-        print("    {}: {!s}".format(inverse_red('‼'), message), file=self._file)
+        print("{}: {!s}".format(inverse_red('‼'), message), file=self._file)
 
     def on_run_start(self, runner, timestamp=None):
         UL, hor, vert, UR, LL, LR = _BOXCHARS[1]
-        text = "Start run at {}".format(timestamp)
+        text = "Start run at {0:{width}s}".format(timestamp, width=WIDTH-17)
         tt = "{}{}{}".format(UL, hor*(len(text)+2), UR)
         bt = "{}{}{}".format(LL, hor*(len(text)+2), LR)
         ml = "{} {} {}".format(vert, text, vert)
@@ -204,9 +237,10 @@ class DefaultReportUnicode(DefaultReport):
 
     def on_suite_start(self, testsuite, time=None):
         UL, hor, vert, UR, LL, LR = _BOXCHARS[2]
-        text = "Start {} at {}".format(testsuite.test_name, time.time())
-        tt = "{}{}{}".format(UL, hor*(len(text)+2), UR)
-        bt = "{}{}{}".format(LL, hor*(len(text)+2), LR)
-        ml = "{} {} {}".format(vert, text, vert)
+        ts = str(time.time())
+        nw = WIDTH - len(ts) - 10
+        tt = "{}{}{}".format(UL, hor*(WIDTH-2), UR)
+        bt = "{}{}{}".format(LL, hor*(WIDTH-2), LR)
+        ml = "{0} Suite {1:{width}s} {B}{2}{R}{3}".format(
+              vert, testsuite.test_name, ts, vert, width=nw, B=BLUE, R=RESET)
         print(tt, ml, bt, sep="\n", file=self._file)
-
